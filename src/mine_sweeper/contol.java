7259b103c858acdb34fb1f920d9028f972d81268
Jimmy Zheng <jimmybacktotheroad@hotmail.com>
2015-05-22 21:01:45 -0400

    update the controller

--- src/mine_sweeper/contol.java
+++
@@ -1,8 +0,0 @@
-public class contol{
-	public void new_game(){
-		
-	}
-	public void new_game(int x, int y){
-		
-	}
-}
---
+++ src/mine_sweeper/controller.java
@@ -0,0 +1,10 @@
+package mine_sweeper;
+
+public class controller {
+	public void new_game() {
+	}
+
+	public void new_game(int x, int y) {
+	}
+	
+}
