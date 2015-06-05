e72e8b25e0f7c810af198e08c4399c90ae523488
kunxiaozheng <kunxiaozheng@gmail.com>
2015-06-05 01:09:06 -0400

    button layout done

--- src/mine_sweeper/Game_GUI.java
+++ src/mine_sweeper/Game_GUI.java
@@ -19,26 +19,34 @@ public class Game_GUI extends JFrame implements ActionListener {
 	JButton midButton = new JButton("Mid");
 	JButton hardButton = new JButton("Hard");
 	JButton resetButton = new JButton("Reset");
-	
+
 	JButton temp;
 
 	Game_GUI(int x, int y) {
 
 		super("Mine Sweeper");
+		
 		setBounds(700, 400, 300, 200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		int length = x * 30 + 1;
+		int width = y * 30 + 1;
 
 		Container contain = this.getContentPane();
-
+		if (length > 300) {
+			this.setPreferredSize(new Dimension(length, width + 80));
+		} else {
+			this.setPreferredSize(new Dimension(300, width + 80));
+		}
+		
 		topPanel.setLayout(new FlowLayout());
 		topPanel.setPreferredSize(new Dimension(300, 40));
-		
+
 		topPanel.add(easyButton);
 		topPanel.add(midButton);
 		topPanel.add(hardButton);
 		topPanel.add(resetButton);
 
-		bottomPanel.setPreferredSize(new Dimension(x*10+1, y*1+1));
 		buttonPanel.setLayout(new GridLayout(x, y, 1, 1));
 		for (int i = 0; i < x * y; i++) {
 			temp = new JButton();
@@ -46,14 +54,14 @@ public class Game_GUI extends JFrame implements ActionListener {
 			buttonPanel.add(buttonList.get(i));
 		}
 
-
 		bottomPanel.setPreferredSize(new Dimension(300, 40));
+
+		easyButton.addActionListener(this);
 		
 		contain.add(topPanel, BorderLayout.NORTH);
 		contain.add(buttonPanel, BorderLayout.CENTER);
 		contain.add(bottomPanel, BorderLayout.SOUTH);
-		
-		this.setPreferredSize(new Dimension(300, 500));
+
 		this.pack();
 		setVisible(true);
 	}
@@ -63,8 +71,13 @@ public class Game_GUI extends JFrame implements ActionListener {
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==easyButton){
+			this.dispose();
+			new Game_GUI(20,20);
+		}else if(e.getSource()==easyButton){
+			
+		}
 
 	}
 }
